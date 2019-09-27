fbe606f393d7ade7f6c56fbc142adf7a278018c1
Thomas R <thomas.rousseau@protonmail.com>
2019-09-27 09:56:47 +0200

    wip

--- src/app/degree/degree-form/degree-form.component.ts
+++ src/app/degree/degree-form/degree-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AbstractControl, FormBuilder, Validators} from '@angular/forms';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Degree} from '../../models/degree';
 import {tap} from 'rxjs/operators';
 import {DegreeApiService} from '../../services/degree-api.service';
@@ -18,7 +18,8 @@ export class DegreeFormComponent implements OnInit {
   constructor(private readonly fb: FormBuilder,
               private readonly route: ActivatedRoute,
               private readonly apiDegree: DegreeApiService,
-              private readonly apiCandidate: CandidateApiService) { }
+              private readonly apiCandidate: CandidateApiService,
+              private readonly router: Router) { }
 
   get isNew(): boolean { return this.degreeId === null; }
 
@@ -28,7 +29,7 @@ export class DegreeFormComponent implements OnInit {
 
   candidate: Candidate = null;
   degreeId: number = null;
-  editedDegree: Degree;
+  // editedDegree: Degree;
 
   form = this.fb.group({
     // id: [''],
@@ -41,53 +42,64 @@ export class DegreeFormComponent implements OnInit {
   }
 
   onSubmit() {
-    // TODO: determine post/put
-    // TODO: send api
-
-    const degree = this.form.value;
+    // const degree = this.form.value;
 
     let request = null;
 
     if (this.isNew) {
-      request = this.apiDegree.create(degree).subscribe(data => {
-        this.editedDegree = data;
-        this.setToCandidate(this.editedDegree);
-
+      // request = this.apiDegree.create(degree).subscribe(data => {
+        // this.editedDegree = data;
+        // this.setToCandidate(this.editedDegree);
+        // this.router.navigate([this.router.getCurrentNavigation()]);
         // TODO: Reload current route
-      });
+      // });
+      request =
+        this.route.params
+          .subscribe(params => {
+            // FIXME: Should manage not found (interceptor).
+            if (params.id) {
+              this
+                .apiCandidate
+                .getOne(params.id)
+                .pipe(tap(e => this.candidate = e))
+                .subscribe(e => {
+                  this.candidate = e;
+                  const degree = this.form.value;
+                  this.candidate.degrees.push(degree);
+                  console.log(this.candidate);
+                  this.apiCandidate.edit(this.candidate.id, this.candidate).subscribe();                  }
+                );
+            }
+          });
     } else {
       // TODO : Faire fonctionner l'édition
-      // request = this.api.edit(this.id, degree).subscribe(data => {
-      //   this.editedDegree = data;
-      //   // TODO: Reload current route
-      // });
     }
 
     return request;
   }
 
   /** Initializes from route parameters. */
-  private setToCandidate(degree) {
-    return this.route
-      .params
-      .subscribe(params => {
-        // FIXME: Should manage not found (interceptor).
-        if (params.id) {
-          this
-            .apiCandidate
-            .getOne(params.id)
-            .pipe(tap(e => this.candidate = e))
-            .subscribe(e => {
-              this.candidate = e;
-              this.editCandidate(degree, this.candidate);
-              }
-            );
-        }
-      });
-  }
+  // private setToCandidate(degree) {
+  //   return this.route
+  //     .params
+  //     .subscribe(params => {
+  //       // FIXME: Should manage not found (interceptor).
+  //       if (params.id) {
+  //         this
+  //           .apiCandidate
+  //           .getOne(params.id)
+  //           .pipe(tap(e => this.candidate = e))
+  //           .subscribe(e => {
+  //             this.candidate = e;
+  //             this.editCandidate(degree, this.candidate);
+  //             }
+  //           );
+  //       }
+  //     });
+  // }
 
-  private editCandidate(degree, candidate) {
-    candidate.degrees.push(degree);
-    this.apiCandidate.edit(candidate.id, candidate).subscribe();
-  }
+  // private editCandidate(degree, candidate) {
+  //   candidate.degrees.push(degree);
+  //   this.apiCandidate.edit(candidate.id, candidate).subscribe();
+  // }
 }
